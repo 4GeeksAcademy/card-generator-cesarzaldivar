61bbdb1b76a8e127514aa72bf099ebc4b1b6aac8
César Jovanni Zaldivar Camps <132796806+Cesarzaldivar@users.noreply.github.com>
2024-05-13 02:08:07 +0000

    card generator cesarZaldivar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "K", "Q", "A"];
+const icons = ["♦", "♥", "♠", "♣"];
+
+const randomNumberArray = arr => {
+  return Math.floor(Math.random() * arr.length);
+};
+const numberRandom = numbers[randomNumberArray(numbers)];
+
+const randomIconArray = arr => {
+  return Math.floor(Math.random() * arr.length);
+};
+
+const iconRandom = icons[randomIconArray(icons)];
+
+let textColor = "text-dark";
+if (iconRandom == "♥" || iconRandom == "♦") {
+  textColor = "text-danger";
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const appElement = document.getElementById("app");
+  appElement.innerHTML = `
+  <div class="container d-flex flex-column justify-content-between  mt-5 card" style="width: 24rem; height: 35rem;">
+  <div id="iconHigh">
+    <h1 class="${textColor}" style="font-size: 100px;">
+      ${iconRandom}
+    </h1>
+  </div>
+  <div id="number">
+    <h1  style="font-size: 150px; text-align: center;">
+      ${numberRandom}
+    </h1>
+  </div>
+  <div id="iconDown" class="text-end">
+    <h1 class="${textColor}" style="font-size: 100px;">
+      ${iconRandom}
+    </h1>
+  </div>
+</div>
+  `;
 };
